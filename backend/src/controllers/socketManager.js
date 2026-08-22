f0f700154a8e390919be56c29da4e24fca8105bf
CoderMak25 <codewithmayankamble@gmail.com>
2026-08-22 23:47:52 +0530

    understood the join-call code

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -10,6 +10,23 @@ export const connectToSocket = (server)=>{
     io.on("connection",(socket)=>{
 
         socket.on("join-call",(path)=>{
+            if(connections[path] == undefined){
+                connections[path] = []
+            }
+            connections[path].push(socket.id);
+            timeOnline[socket.id] = new Date();
+
+            for(let a = 0; a< connections[path].length; a++){
+                io.to(connections[path][a]).emit("user-joined",socket.id, connections[path]);
+            }
+
+            if(messsages[path] != undefined){
+                for(let a =0;a < messages[path].length; ++a){
+                    io.to(socket.id).emit("chat-message", messages[path][a]['data'], messages[path][a]['sender'],messages[path][a]['socket-id-sender']);
+                }
+            }
+
+
 
 
         })
